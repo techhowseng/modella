a7f984bcc97566eb6937258f034eb745e3ee7a7d
Gabriel Micah <gabrielsuave17@gmail.com>
2023-02-08 16:14:39 +0100

    update send email function

--- src/services/MailService.ts
+++ src/services/MailService.ts
@@ -1,29 +1,50 @@
 import sgMail, { MailDataRequired } from "@sendgrid/mail";
 import isHtml from 'is-html';
 import path from "path";
+// import _jade from "jade";
 import { createWriteStream, readFile } from "fs";
 import dotenv from "dotenv";
 dotenv.config();
 
-export default function sendMail (to, subject, payload ) {
+export default function sendMail (to: any, subject: string, payload: string ) {
   const apiKey = process.env.SENDGRID_API_KEY;
   sgMail.setApiKey(apiKey);
 
-  const message: MailDataRequired = {
-    to,
-    from: process.env.MAIL_SENDER,
-    subject,
-    text: "skfdjglksdfgskjfds",
-    // html: ""
-  };
+  const template = path.join(process.cwd(), "email-templates", "index.jade");
+  readFile(template, 'utf8', async function (err, file) {
 
-  sgMail
-    .send(message)
-    .then(() => {
-      console.info("Message send success");
+    if (err) {
+      console.error('send confirmation email', err);
+      throw new Error('Error sending confirmation email');
+    }
+    const context = {
+      subject: '',
+      title: '',
+      header: '',
+      bodyTitle: '',
+      bodyDescription: '',
+      buttonText: '',
+      buttonLink: '',
+    };
+    // const compiledTmpl = _jade.compile(file, context);
+    // const html = compiledTmpl(context);
+    const html = isHtml(payload) ? payload : file;
+    const msg: MailDataRequired = {
+      to,
+      from: process.env.MAIL_SENDER,
+      subject,
+      html,
+    };
 
-    })
-    .catch((err) => {
-      console.error(`Message send failed: ${err}`);
-    });
+    try {
+      const emailRes = await sgMail.send(msg);
+      console.log('email result: ', emailRes);
+
+    } catch (error) {
+      console.error(error);
+      if (error.response) {
+        console.error(error.response.body);
+      }
+    }
+  });
 }
