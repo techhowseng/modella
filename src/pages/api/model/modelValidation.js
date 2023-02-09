af1631530e77c30d85d05edf8a54c836b02a9798
obi walker <obinnawalker@gmail.com>
2023-02-09 23:31:44 +0100

    fix model validation - fox password error - change verify string to uuid

--- src/pages/api/model/modelValidation.js
+++ src/pages/api/model/modelValidation.js
@@ -10,23 +10,23 @@ export const validateModel = initMiddleware(
     .withMessage('is not long enough.'),
     check('lastname').isLength({min:2})
     .withMessage('is not long enough.'),
-    check('height').isLength({min:3})
+    check('height').isLength({min:3}).optional({ nullable: true })
     .withMessage('is not descriptive enough.'),
-    check('bust').isLength({min:2})
+    check('bust').isLength({min:2}).optional({ nullable: true })
     .withMessage('is not descriptive enough.'),
-    check('waist').isLength({min:2})
+    check('waist').isLength({min:2}).optional({ nullable: true })
     .withMessage('is not descriptive enough.'),
-    check('hip').isLength({min:2})
+    check('hip').isLength({min:2}).optional({ nullable: true })
     .withMessage('is not descriptive enough.'),
-    check('shoeSize').isLength({min:2})
+    check('shoeSize').isLength({min:2}).optional({ nullable: true })
     .withMessage('is not descriptive enough.'),
-    check('weight').isLength({min:2})
+    check('weight').isLength({min:2}).optional({ nullable: true })
     .withMessage('is not descriptive enough.'),
-    check('complexion').isLength({min:4})
+    check('complexion').isLength({min:4}).optional({ nullable: true })
     .withMessage('is not descriptive enough.'),
-    check('DOB').isLength({min:5})
+    check('DOB').isLength({min:5}).optional({ nullable: true })
     .withMessage('is not written in the right format.'),
-    check('social').isObject()
+    check('social').isObject().optional({ nullable: true })
     .withMessage('should be sent as an object.'),
     check('state').isLength({min:2})
     .withMessage('is not long enough.'),
@@ -34,7 +34,7 @@ export const validateModel = initMiddleware(
     .withMessage('is not descriptive enough.'),
     check('address').isLength({min:7})
     .withMessage('is too short.'),,
-    check('isAvailable').isBoolean()
+    check('isAvailable').isBoolean().optional({ nullable: true })
     .withMessage('should be true or false.'),,
     check('phone').isObject()
     .withMessage('should be sent as an object.'),
--- src/pages/api/user/repository/index.ts
+++ src/pages/api/user/repository/index.ts
@@ -2,10 +2,10 @@ import { PrismaClient } from "@prisma/client";
 import prisma from "lib/prisma";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import { uuid } from 'uuidv4';
 import UserServices, { TUser } from "../service";
 import SessionServices from "../../session/service";
-import { getUser, randomStringGenerator } from "../../../../helper/util";
-import { checkExistingUser, existsInDB } from "../helper";
+import { getUser } from "../../../../helper/util";
 import NotificationService from "../../../../services/NotificationService";
 import { ResponseService } from "../../../../services/ResponseService";
 import { EntityExistsError } from "helper/errors";
@@ -24,6 +24,7 @@ export default class UserRepository {
     try {
       const inputs = req.body;
       inputs.email = inputs.email.toLowerCase();
+      inputs.password = bcrypt.hashSync(inputs.password, 8)
       // this should be in service
       const existingEmail = await prisma.user.findFirst({
         where: {
@@ -36,7 +37,7 @@ export default class UserRepository {
           new EntityExistsError("user", inputs.email)
         );
       }
-      const verificationToken = randomStringGenerator();
+      const verificationToken = uuid();
       const newVerification = await UserServices.createVerificationToken(
         inputs,
         verificationToken
@@ -49,8 +50,7 @@ export default class UserRepository {
         res,
         200,
         "Please verify your email address within 10 minutes",
-        newVerification
-      );
+        (({ token, email}) => ({token, email}))(newVerification));
     } catch (err) {
       return ResponseService.sendError(err, res);
     }
@@ -166,7 +166,13 @@ export default class UserRepository {
       const verifiedUser = await UserServices.verifyToken(res, token);
       if (verifiedUser) {
         const { email, password, type } = verifiedUser as any;
-        const user = await UserServices.createUser(res, email, password, type);
+        const user = await UserServices.createUser(
+          res,
+          email,
+          bcrypt.hashSync(password, 8),
+          type,
+          true
+          );
         await UserServices.deleteVerificationToken(res, email);
         const jwtToken = jwt.sign(user, JWT_KEY, {
           expiresIn: "24hr",
