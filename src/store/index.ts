fc8367156698b335c4a8cb08c1e1c458920c97cd
Gabriel Micah <gabrielsuave17@gmail.com>
2023-03-02 13:05:26 +0100

    get clients jobs

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,11 @@
 import { Action, configureStore, ThunkAction } from "@reduxjs/toolkit";
 import authReducer from "features/Auth/slice";
+import clientJobsReducer from "features/ClientAccount/slice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    clientJobs: clientJobsReducer,
   },
 });
 
