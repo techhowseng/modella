19009b89ac967d8a55f91a16560f50e4cd137f65
Gabriel Micah <gabrielsuave17@gmail.com>
2023-05-04 23:32:51 +0100

    add flox-bite

--- src/lib/functions.ts
+++ src/lib/functions.ts
@@ -1,3 +1,11 @@
+import jwt_decode from "jwt-decode";
+
 export function classNames(...classes: any[]) {
   return classes.filter(Boolean).join(" ");
 }
+
+export const isTokenExpired = (token: string) => {
+  if (!token) return true;
+  const decoded: any = jwt_decode(token);
+  return decoded.exp * 1000 < new Date().getTime();
+};
