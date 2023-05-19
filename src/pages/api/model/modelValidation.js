9885b65647ac065c8e0718f28cb8b5346fbe3d14
Obi Walker <obinnawalker@gmail.com>
2023-05-20 00:21:40 +0100

    Update modelValidation.js

--- src/pages/api/model/modelValidation.js
+++ src/pages/api/model/modelValidation.js
@@ -51,7 +51,29 @@ export const validateCreateModel = initMiddleware(
 
 export const validateUpdateModel = initMiddleware(
   validateMiddleware([
-update get all media by user method
+    check('firstname').isLength({ min: 2 })
+      .optional({ nullable: true })
+      .withMessage('is not long enough.'),
+    check('lastname').isLength({ min: 2 })
+      .optional({ nullable: true })
+      .withMessage('is not long enough.'),
+    check('height').isNumeric().isLength({ min: 3 }).optional({ nullable: true })
+      .withMessage('is not descriptive enough.'),
+    check('bust').isNumeric().isLength({ min: 2 }).optional({ nullable: true })
+      .withMessage('is not descriptive enough.'),
+    check('waist').isNumeric().isLength({ min: 2 }).optional({ nullable: true })
+      .withMessage('is not descriptive enough.'),
+    check('hip').isNumeric().isLength({ min: 2 }).optional({ nullable: true })
+      .withMessage('is not descriptive enough.'),
+    check('shoeSize').isNumeric().optional({ nullable: true })
+      .withMessage('is not descriptive enough.'),
+    check('weight').isLength({ min: 2 }).optional({ nullable: true })
+      .withMessage('is not descriptive enough.'),
+    check('complexion').isLength({ min: 4 }).optional({ nullable: true })
+      .withMessage('is not descriptive enough.'),
+    check('dob').isLength({ min: 5 })
+      .optional({ nullable: true })
+      .withMessage('is not written in the right format.'),
     check('social').isObject().optional({ nullable: true })
       .withMessage('should be sent as an object.'),
     check('state').isLength({ min: 2 })
