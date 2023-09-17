d177765f16ab7b1ea0ee744af2e6834595449894
Gabriel Micah <gabrielsuave17@gmail.com>
2023-09-17 04:28:48 +0100

    Add CORS control

--- next.config.js
+++ next.config.js
@@ -2,4 +2,18 @@ module.exports = {
     images: {
         domains: ["res.cloudinary.com", "randomuser.me", "images.unsplash.com", "www.gravatar.com"],
     },
+    async headers() {
+        return [
+            {
+                // matching all API routes
+                source: "/api/:path*",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: "https://manikeens-lake.vercel.app/" },
+                    { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+                ]
+            }
+        ]
+    }
 }
